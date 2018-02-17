434cf0799eb9e51d1a211478144b77fc0b4258f5
Ronald Jefrey Moreno Mora <morenoronald7@gmail.com>
2018-02-17 12:42:32 -0500

    cambios ejemplo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,11 +92,19 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
             </div>
         </div>
         <div class="container">
             <h2>Estructuras</h2>
-            
+
             <h2>Estructuras</h2>
 
         </div>
